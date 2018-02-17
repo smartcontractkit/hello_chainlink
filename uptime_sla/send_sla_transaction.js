4a83f4534ec38bed9cf6aa0551f380e071d70f2f
Steve Ellis <email@steveell.is>
2018-02-17 11:42:46 -0800

    fix send sla transaction command

--- uptime_sla/send_sla_transaction.js
+++ uptime_sla/send_sla_transaction.js
@@ -1,15 +1,22 @@
 #!/usr/bin/env node
 
-var Web3            = require('web3'),
-    contract        = require("truffle-contract"),
-    path            = require('path')
-    UptimeSLAJSON   = require(path.join(__dirname, 'build/contracts/EthLog.json'));
+let Web3 = require('web3');
+let TruffleContract = require("truffle-contract");
+let UptimeSLAJSON = require('./build/contracts/UptimeSLA.json');
 
-var provider = new Web3.providers.HttpProvider("http://localhost:18545");
-var UptimeSLA = artifacts.require("./UptimeSLA.sol");
+var provider = new Web3.providers.HttpProvider("http://127.0.0.1:18545");
+var UptimeSLA = TruffleContract(UptimeSLAJSON);
 UptimeSLA.setProvider(provider);
+if (typeof UptimeSLA.currentProvider.sendAsync !== "function") {
+  UptimeSLA.currentProvider.sendAsync = function() {
+    return UptimeSLA.currentProvider.send.apply(
+      UptimeSLA.currentProvider, arguments
+    );
+  };
+}
 var devnetAddress = "0x9CA9d2D5E04012C9Ed24C0e513C9bfAa4A2dD77f";
 
 UptimeSLA.deployed().then(function(instance) {
-  return instance.updateUptime("0", {from: devnetAddress});
+  console.log("deployed!")
+  return instance.updateUptime('0', {from: devnetAddress});
 }).then(console.log, console.log);
