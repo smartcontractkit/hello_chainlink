8d930a5543813dd1a6aee151dfa1bc1791a80f7e
Dimitri Roche <dimroc@gmail.com>
2018-02-22 09:07:57 -0500

    Refactor uptime_sla migration to use chainlinkDeployer

--- uptime_sla/migrations/3_uptime_sla.js
+++ uptime_sla/migrations/3_uptime_sla.js
@@ -1,33 +1,22 @@
-let fs = require("fs");
-let request = require("request");
-
+let chainlinkDeployer = require("../chainlink_deployer.js");
 let UptimeSLA = artifacts.require("./UptimeSLA.sol");
 let Oracle = artifacts.require("../node_modules/smartcontractkit/chainlink/solidity/contracts/Oracle.sol");
 
-module.exports = function(deployer) {
+module.exports = function(truffleDeployer) {
   let client = "0x542B68aE7029b7212A5223ec2867c6a94703BeE3";
   let serviceProvider = "0xB16E8460cCd76aEC437ca74891D3D358EA7d1d88";
-  let url = "http://chainlink:twochains@localhost:6688/v2/jobs";
 
-  fs.readFile("http_json_x10000_job.json", 'utf8', (err, file) => {
-    let data = JSON.parse(file);
-    console.log(`Posting to ${url}:\n`, data);
-    request.post(url, {json: data},
-      function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-              console.log(`Deploying UptimeSLA:`)
-              console.log(`\tjob: ${body.id}`);
-              console.log(`\tclient: ${client}`);
-              console.log(`\tservice provider: ${serviceProvider}`);
+  chainlinkDeployer.job("http_json_x10000_job.json", function(error, response, body) {
+    console.log(`Deploying UptimeSLA:`)
+    console.log(`\tjob: ${body.id}`);
+    console.log(`\tclient: ${client}`);
+    console.log(`\tservice provider: ${serviceProvider}`);
 
-              deployer.deploy(UptimeSLA, client, serviceProvider, 1518857382, Oracle.address, body.id, {
-                value: 1000000000
-              });
-            } else {
-              console.log("chainlink error:", error);
-              console.log("response status:", response.statusCode);
-            }
-        }
-    );
+    truffleDeployer.deploy(UptimeSLA, client, serviceProvider, 1518857382, Oracle.address, body.id, {
+      value: 1000000000
+    });
+  }, function(error, response) {
+    console.log("chainlink error:", error);
+    console.log("response status:", response.statusCode);
   });
 };
